d8cb83febf7b1312a2eee66198ff6a7f87da182e
cristinaitako <125576531+cristinaitako@users.noreply.github.com>
2024-06-26 17:56:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ const perguntas = [
     {
         enunciado: "Em sua cidade ocorrem assédios e desrespeitos às mulheres e aos assentos prioritários?", 
         alternativas: [ 
-            "alternativa1",
-            "alternativa2"
+            "Há um impacto muito grande no meio ambiente, causando prejuízos ao solo, água e atmosfera.",
+             "A produção das espécies  é maior, reduz o controle de pragas, aumenta a distribuição da produção."
+
         ]
     }
 ]
