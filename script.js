10161b743e3d4f721ffd53d0e05271f53b3c1f30
cristinaitako <125576531+cristinaitako@users.noreply.github.com>
2024-06-26 17:42:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas= document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [Em sua cidade ocorrem assédios e desrespeitos às mulheres e aos assentos prioritários?
+    {
+        enunciado: "Pergunta1", 
+        alternativas: [ 
+            "alternativa1",
+            "alternativa2"
+        ]
+    }
+]
+const perguntas = [
+    {
+        enunciado: "Pergunta2",
+        alternativas: [
+            "alternativa1",
+            "alternativa2"
+        ]
+    }
+]
+const perguntas = [
+    enunciado: "Pergunta3",
+    alternativas:[
+        {
+            "alternativa1",
+            "alternativa2"
+        }
+    ]
+]
